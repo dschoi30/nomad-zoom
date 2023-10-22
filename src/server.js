bf32235a1d7eff3d565b48f9b9605f3d71ec9fe7
dschoi30 <ds0631@gmail.com>
2023-10-22 10:17:58 +0900

    웹소켓 적용 양방향 메시지 통신 기능 구현

--- src/server.js
+++ src/server.js
@@ -15,15 +15,26 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSocketClose() {
+    console.log("Disconnected from the Browser");
+}
+
+const sockets = [];
+
 wss.on("connection",(socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser");
-    socket.on("close", () => {
-        console.log("Disconnected from the Browser"); 
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
-    socket.on("message", (message) => {
-        console.log(message.toString());
-    });
-    socket.send("Done!!");
+    socket.on("close", onSocketClose);
 });
 
 server.listen(3000, handleListen);
